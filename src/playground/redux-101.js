8c33b70f269b370777abf6fc0a8465fc1f337a30
Syllinger <syllinger@gmail.com>
2017-10-22 15:44:37 -0400

    Add dispatch calls and create action handler

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,50 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = { count:0 }) => {
-  return state;
+/*
+This function sets the initial state, and runs any time 
+an action is triggered on the store.
+*/
+const store = createStore((state = { count:0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    case 'RESET':
+      return { count: 0 };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// Action: an object that gets sent to the store
+
+// Increment
+/*
+When the dispatch method is called on the store, it causes the 
+function declared when the store was created using createStore 
+to execute, passing the object declared. When the createStore 
+function is executed, this object is then passed into the second 
+argument "action" and we use a switch statement to handle each 
+"action type."
+*/
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+// Decrement
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
 });
 
 console.log(store.getState());
