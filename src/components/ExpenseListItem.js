8022126736758d36c4ed477086ddb5b4c153ff4e
Syllinger <syllinger@gmail.com>
2017-10-22 23:44:06 -0400

    Added comments to ExpenseListItem detailing connect method usage

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -10,4 +10,10 @@ const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
   </div>
 );
 
+/* 
+First function argument allows us to map state to props, but in this 
+case we do not wish to map state. We use "connect" without supplying 
+an argument to the first function to ensure that "dispatch" method 
+from the store is added to props.
+*/
 export default connect()(ExpenseListItem);
