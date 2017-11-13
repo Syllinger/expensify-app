1e19e39cc2da3d60e55a1c3b6244afccabee9c08
Syllinger <syllinger@gmail.com>
2017-11-12 21:50:45 -0500

    Add test: invalid submission on ExpenseForm component

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,13 @@ test('Should render ExpenseForm with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('Should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
