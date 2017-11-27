4f4ee964f8d51b08bec4d3fc1ea478ac46f2c4c5
Syllinger <syllinger@gmail.com>
2017-11-26 23:17:12 -0500

    Refactor AddExpensePage SFC into CBC to improve testability Add import reference to react-dates library initialize method in ExpenseForm component, as running dev-server would fail at runtime.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,33 +3,39 @@ import {connect} from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import {editExpense, removeExpense} from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  return (
-    <div>
-      <ExpenseForm 
-        expense={props.expense}
-        onSubmit={expense => {
-          /*
-          Was tripped up here because method signature suggests that 
-          the editExpense action requires an id, and an updates object. 
-          Was looking at how to access id being passed up by ExpenseForm, 
-          but "id" is already boudn to the component props variable, as we 
-          mapped it when we called the "connect" method with "mapStateToProps" 
-          where "props.expense" is assigned to the result of the expense 
-          entry found in in the array.
-          */
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push('/');
-        }}
-      />
-      <button 
-        onClick={() => {
-          props.dispatch(removeExpense({id: props.expense.id}));
-          props.history.push('/');
-        }}
-      >Remove</button>
-    </div>
-  );
+export class EditExpensePage extends React.Component {
+  onSubmit = expense => {
+    /*
+    Was tripped up here because method signature suggests that 
+    the editExpense action requires an id, and an updates object. 
+    Was looking at how to access id being passed up by ExpenseForm, 
+    but "id" is already boudn to the component props variable, as we 
+    mapped it when we called the "connect" method with "mapStateToProps" 
+    where "props.expense" is assigned to the result of the expense 
+    entry found in in the array.
+    */
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  };
+
+  onClick = () => {
+    this.props.removeExpense({id: this.props.expense.id});
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm 
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
+        />
+        <button 
+          onClick={this.onClick}
+        >Remove</button>
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state, props) => {
@@ -38,4 +44,9 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: data => dispatch(removeExpense(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
